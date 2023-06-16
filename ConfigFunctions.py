f835583d69ad26b8afffdaf903cb0dc43d8b1651
Aksh Desai <dev.akshdesai@gmail.com>
2023-06-16 18:10:40 +0530

    Added alter and read config completely and partital progress on create config

--- ConfigFunctions.py
+++ ConfigFunctions.py
@@ -1,16 +1,17 @@
 import tkinter as tk
-from  tkinter.filedialog import askdirectory
+from tkinter.filedialog import askdirectory
 
-config_location = "D:\HarshilProject\Output\project_backup.config"
+config_location = "D:\\HarshilBackupProject\\project_backup.config"
 
-def create_config(): #TODO: Add interval and selection adding
+
+def create_config():  # TODO: Add interval and selection adding
     tk.Tk().withdraw()  # prevents an empty tkinter window from appearing
     config_location = askdirectory(title="Select Folder for saving config file")
     config_location = config_location + "/project_backup.config"
 
     try:
         c = open(config_location, "x")
-    except(Exception):
+    except Exception:
         print(Exception)
         c = open(config_location, "w")
         c.write("")
@@ -19,40 +20,44 @@ def create_config(): #TODO: Add interval and selection adding
     a.write("src=" + askdirectory(title="Select Source Folder") + "\n")
     a.write("dst=" + askdirectory(title="Select Destination Folder") + "\n")
 
-    # a.write("int=" + get_number_from_gui() + "\n")
-    # a.write("sel=" + get_subdata_config(src) + "\n")
+    a.write("int=" + "60" + "\n")
+    a.write("sel=" + "Files" + "\n")
 
-def read_config(field): #Done
+
+def read_config(field, return_line = False):  # Done
     config = open(config_location, "r")
     Lines = config.readlines()
 
+    i = 0
     for line in Lines:
-        print("Checking: " + line)
-        if(line.find(field) == 0):
-            return line[4:len(line)]
+        # print("Checking: " + line)
+        if (line.find(field) == 0):
+            if return_line:
+                return i
+            else:
+                return line[4:len(line)]
+
+        i += 1
     return ""
 
-def alter_config():
-    return True
 
-def get_number_from_gui():
-    def submit():
-        number = entry.get()
-        window.destroy()
-        return int(number)
+def alter_config(field, data):  #Done
+    try:
+        lines = []  # Initialize an empty list to store lines
 
-    window = tk.Tk()
-    window.title("Number Input")
+        with open(config_location, 'r') as file:
+            for line in file:
+                lines.append(line)
+        print(lines)
+        print(read_config(field, True))
+        lines[read_config(field, True)] = field + "=" + data + "\n"
 
-    label = tk.Label(window, text="Enter a number: ")
-    label.pack()
 
-    entry = tk.Entry(window)
-    entry.pack()
-
-    submit_button = tk.Button(window, text="Submit", command=submit)
-    submit_button.pack()
-
-    window.mainloop()
-
-print("output: " + read_config("int"))
+        cleaner = open(config_location, 'w')
+        cleaner.write("") #cleaning file
+        appender = open(config_location, 'a')
+        for i in lines:
+            appender.write(i)
+        return True
+    except:
+        return False
